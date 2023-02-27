b7f234039b3eecb0cbc748e29723a88f3073ee4f
tkoszhanov <tilek.koszhanov@mail.ru>
2023-02-27 18:59:22 +0500

    Initial Commit

---
+++ GBShop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GBShop
+//
+//  Created by Tilek Koszhanov on 2/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
